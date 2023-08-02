1dd372447412f82835b1c457e6637b0f97d83beb
Vítor Assis <contatoassisvt@gmail.com>
2023-08-02 18:20:57 -0300

    Create ChatGPT-FrontEnd.py

---
+++ HTML/ChatGPT-FrontEnd.py
@@ -0,0 +1,72 @@
+< !--  Experimento de um site -- >
+<!doctype html>
+<!-- doctype informa ao agente de usuario a versão do html que deve ser renderizada-->
+<html lang="pt-br">
+    <head>
+        <title> pagina de exemplo estrutura basica </title>
+        <meta charset="utf-8">
+        <meta name="author" content="Vítor">
+        <meta name="description" content="descrição bacanuda">
+        <meta name="keywords" content="html5, tecnologia">
+        
+<style>
+    body{
+        background-color: #9abdff;
+    }
+    h1{
+        font-family: 'Franklin Gothic Medium';
+        font-size: 40px;
+        font-style: normal;
+        font-variant: normal;
+        color: rgb(247, 130, 202);
+        background-color: rgb(1, 55, 87);
+        margin-left: 0;
+        margin-top: 50px;
+        margin-bottom: 15px;
+        padding-left: 750px;
+        padding-top: 15px;
+        padding-bottom: 0px;
+        padding-right: 500px;
+        width: 580px;
+        height: 60px;
+        
+    }
+
+    h2{
+        text-transform: uppercase;
+        font-family: 'Franklin Gothic Medium';
+        font-style: italic;
+        color: rgb(255, 255, 255);
+        border-bottom-color: rgb(1, 55, 87);
+        border-bottom-style: dotted;
+        border-bottom-width: 8px;
+        padding: 10px;
+    }
+
+    p{
+        font-variant: normal;
+        font-size: 20px;
+        text-align: left;
+        text-indent: 20px;
+        text-transform: none;
+        margin-bottom: 20px;
+    }
+
+    .destaque{
+        color: rgb(0, 0, 0);
+        background-color: rgb(247, 130, 202);
+    }
+</style>
+    </head>
+    <body>
+        <h1>ChatGPT & Front-end</h1>
+        <h2>Útil ao agradável</h2>
+        
+        <p>A integração do Chat GPT (Generative Pre-trained Transformer) no aprendizado de front-end pode ser uma ferramenta muito útil para estudantes e desenvolvedores que buscam aprimorar suas habilidades nessa área.</p>
+        <p class="destaque">O Chat GPT é uma tecnologia de inteligência artificial que permite criar conversas com respostas em linguagem natural, como se fosse uma conversa com um ser humano real. Isso significa que ele pode ser usado como um recurso para auxiliar no aprendizado de front-end, ajudando a responder perguntas, fornecendo dicas e solucionando problemas em tempo real.</p>
+        <p>Com a integração do Chat GPT, os estudantes de front-end podem ter uma experiência mais personalizada e interativa, recebendo feedback instantâneo e em linguagem natural. Isso pode ajudar a acelerar o processo de aprendizado, uma vez que os alunos terão um ambiente mais dinâmico e imersivo para praticar e testar seus conhecimentos.</p>
+        <p class="destaque">Além disso, o Chat GPT pode ser programado para fornecer informações específicas sobre HTML, CSS e JavaScript, por exemplo, e pode ser uma ferramenta útil para ajudar os alunos a identificar e corrigir erros de sintaxe. Isso pode economizar tempo e aumentar a eficiência do processo de aprendizado, permitindo que os estudantes se concentrem em outras áreas que requerem mais atenção.</p>
+        <p>No geral, a integração do Chat GPT no aprendizado de front-end pode ser uma maneira eficaz de melhorar a qualidade e a eficiência do processo de aprendizado, oferecendo aos estudantes uma experiência mais personalizada e interativa. Isso pode ser especialmente útil para alunos que preferem um ambiente de aprendizado mais prático e visual, bem como para aqueles que precisam de feedback imediato para corrigir erros e aprimorar suas habilidades.</p>
+    
+    </body>
+</html>
